43c48deea104c3a64d1c3bf45fe9209f81e05180
amlm155 <3431673637@qq.com>
2026-05-01 16:10:36 +0800

    UI基础逻辑实现

---
+++ Pick-Light-From-Dark/Assets/Scripts/UI/AboutUsPanel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutUsPanel : BasePanel
+{
+    public override void HideMe()
+    {
+        
+    }
+
+    public override void ShowMe()
+    {
+        
+    }
+
+    protected override void ClickBtn(string btnName)
+    {
+        switch (btnName)
+
+        {
+            case "BackBtn":
+                UIMgr.Instance.HidePanel<AboutUsPanel>();
+                UIMgr.Instance.ShowPanel<BeginPanel>();
+                break;
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Pick-Light-From-Dark/Assets/Scripts/UI/SaveGamePanel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveGamePanel : BasePanel
+{
+    public override void HideMe()
+    {
+        
+    }
+
+    public override void ShowMe()
+    {
+       
+    }
+    protected override void ClickBtn(string btnName)
+    {
+        switch (btnName)
+
+        {
+            case "BackBtn":
+                UIMgr.Instance.HidePanel<SaveGamePanel>();
+                UIMgr.Instance.ShowPanel<BeginPanel>();
+                break;
+            case "SaveGameBtn1":
+                break;
+            case "SaveGameBtn2":
+                break;
+            case "SaveGameBtn3":
+                break;
+            case "SaveGameBtn4":
+                break;
+        }
+
+
+
+
+
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
